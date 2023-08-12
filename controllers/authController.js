6f02988d940143434086d9215184a5f9a94df7c7
lamptissue <ncarribine@hotmail.co.uk>
2023-08-12 15:24:42 +0100

    updating code

--- controllers/authController.js
+++ controllers/authController.js
@@ -60,9 +60,9 @@ exports.login = async (req, res) => {
     res.status(200).json({
       status: 'success',
       token,
-      // data: {
-      //   user,
-      // },
+      data: {
+        user,
+      },
     });
   } catch (err) {
     res.status(500).json({
