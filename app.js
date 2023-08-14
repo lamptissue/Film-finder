3d8324ffda76f293a0cffea4faf1348fdf5ea392
lamptissue <ncarribine@hotmail.co.uk>
2023-08-14 10:16:15 +0100

    clean code and bug fixes

--- app.js
+++ app.js
@@ -146,43 +146,6 @@ app.get('/api/users', (req, res) => {
 app.post('/api/users/signup', authController.signup);
 app.post('/api/users/login', authController.login);
 
-//Update user info
-// app.post(
-//   '/api/users',
-//   passport.authenticate('jwt', { session: false }),
-//   (req, res) => {
-//     users.push(req.body);
-//     fs.writeFile(`${__dirname}/users.json`, JSON.stringify(users), (err) => {
-//       res.status(201).json({
-//         status: 'success',
-//         data: {
-//           users,
-//         },
-//       });
-//     });
-//   }
-// );
-
-// app.get(
-//   '/api/users/:username',
-//   passport.authenticate('jwt', { session: false }),
-//   (req, res) => {
-//     const user = User.findOne((el) => el.username === req.params.username);
-//     if (!user) {
-//       res.status(404).json({
-//         status: 'Error',
-//         message: 'Username does not exist',
-//       });
-//     }
-//     res.status(200).json({
-//       status: 'Success',
-//       data: {
-//         user,
-//       },
-//     });
-//   }
-// );
-
 app.get(
   '/api/users/:username',
   passport.authenticate('jwt', { session: false }),
@@ -213,26 +176,6 @@ app.get(
   }
 );
 
-//update user details
-// app.patch(
-//   '/api/users/:username',
-//   passport.authenticate('jwt', { session: false }),
-//   (req, res) => {
-//     const user = User.findOne((el) => el.username === req.params.username);
-//     if (!user) {
-//       res.status(404).json({
-//         status: 'Error',
-//         message: 'Username does not exist',
-//       });
-//     }
-//     res.status(200).json({
-//       status: 'success',
-//       data: {
-//         users: 'Updated user here',
-//       },
-//     });
-//   }
-// );
 app.patch(
   '/api/users/:username',
   passport.authenticate('jwt', { session: false }),
@@ -275,10 +218,25 @@ app.patch(
   async (req, res) => {
     try {
       const filmIdToAdd = req.params.filmid;
+      const username = req.params.username;
 
+      // Check if the film already exists in the user's favorite films
+      const doesFilmExist = await User.exists({
+        username,
+        favouriteFilms: filmIdToAdd,
+      });
+
+      if (doesFilmExist) {
+        return res.status(200).json({
+          status: 'success',
+          message: 'Film is already in favourites',
+        });
+      }
+
+      // If the film doesn't exist in the user's favorites, add it
       const user = await User.findOneAndUpdate(
-        { username: req.params.username },
-        { $push: { favouriteFilms: filmIdToAdd } },
+        { username },
+        { $addToSet: { favouriteFilms: filmIdToAdd } },
         { new: true, upsert: true }
       );
 
@@ -291,9 +249,7 @@ app.patch(
 
       res.status(200).json({
         status: 'success',
-        message: user.favouriteFilms.includes(filmIdToAdd)
-          ? 'Film is already in favorites'
-          : 'Film has been added',
+        message: 'Film has been added',
       });
     } catch (error) {
       console.error(error);
@@ -336,26 +292,34 @@ app.delete(
 app.delete(
   '/api/users/:username/films/:filmid',
   passport.authenticate('jwt', { session: false }),
-  (req, res) => {
-    const user = User.findOneAndUpdate(
-      { username: req.params.username },
-      {
-        $pull: { favouriteFilms: req.params.filmid },
-      },
-      { new: true }
-    );
+  async (req, res) => {
+    try {
+      const user = await User.findOneAndUpdate(
+        { username: req.params.username },
+        {
+          $pull: { favouriteFilms: req.params.filmid },
+        },
+        { new: true }
+      );
 
-    if (!user) {
-      res.status(404).json({
+      if (!user) {
+        return res.status(404).json({
+          status: 'Error',
+          message: 'Username does not exist',
+        });
+      }
+
+      res.status(204).json({
+        status: 'success',
+        message: 'Film has been removed',
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({
         status: 'Error',
-        message: 'Username does not exist',
+        message: 'Internal server error',
       });
     }
-
-    res.status(204).json({
-      status: 'success',
-      message: 'Film has been removed',
-    });
   }
 );
 
